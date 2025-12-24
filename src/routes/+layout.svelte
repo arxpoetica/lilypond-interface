8d3231a78c97ba62b3b58f5b51997a7dd44d4261
Robert Hall <arxpoetica@users.noreply.github.com>
2025-12-24 15:05:31 -0700

    Daisy UI

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,5 +5,12 @@
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<div class="bg-base-50 min-h-screen text-4xl">
+	<div class="container mx-auto max-w-4xl px-4 py-8">
+		{@render children()}
+	</div>
+</div>
